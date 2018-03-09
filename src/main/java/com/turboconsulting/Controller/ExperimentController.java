461e8a15dcf53297ea3b10c72d4ece4f63892ba8
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-03-09 15:15:32 +0000

    Created Empty Controller classes, just debating on how to incorporate id mappings

---
+++ src/main/java/com/turboconsulting/Controller/ExperimentController.java
@@ -0,0 +1,9 @@
+package com.turboconsulting.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/experiments")
+public class ExperimentController {
+}
---
+++ src/main/java/com/turboconsulting/Controller/ProfileController.java
@@ -0,0 +1,11 @@
+package com.turboconsulting.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+
+}
