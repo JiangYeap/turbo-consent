46a4e8dde2f8db27c6cb48211643279ed68961b0
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-04-06 09:14:36 +0100

    Visitors controller, new url=http://localhost:8080/visitors?aID=35

--- src/main/java/com/turboconsulting/Controller/VisitorsController.java
+++ src/main/java/com/turboconsulting/Controller/VisitorsController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class VisitorsController {
@@ -13,9 +14,9 @@ public class VisitorsController {
     @Autowired
     private ConsentService consentService;
 
-    @GetMapping("/{aID}/visitors")
+    @GetMapping("/visitors")
     public String visitorsPage(Model m,
-                               @PathVariable(value="aID") int aID)  {
+                               @RequestParam(value="aID") int aID)  {
         m.addAttribute("visitors", consentService.getAccountsVisitors(aID));
         return "visitors";
     }
