fa54284b03693ef85eb9052a3a1c4708170482b7
Unknown <fh16413@my.bristol.ac.uk>
2018-04-07 21:04:49 +0100

    Allow users to go to /visitors or /settings directly from Login

--- src/main/java/com/turboconsulting/Controller/SettingsController.java
+++ src/main/java/com/turboconsulting/Controller/SettingsController.java
@@ -1,8 +1,12 @@
 package com.turboconsulting.Controller;
 
 import com.turboconsulting.Entity.ConsentLevel;
+import com.turboconsulting.Security.MyUser;
+import com.turboconsulting.Security.MyUserDetailsService;
 import com.turboconsulting.Service.ConsentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,9 @@ public class SettingsController {
     @Autowired
     private ConsentService consentService;
 
+    @Autowired
+    private MyUserDetailsService userService;
+
     @GetMapping("/settings")
     public String homePage(ModelMap m,
                            @RequestParam(value = "update", required = false) boolean updateSuccess) {
@@ -30,8 +37,14 @@ public class SettingsController {
 
     @PostMapping("/settings/updateConsent")
     public String updateConsent(ModelMap m, @ModelAttribute("consent") String c)  {
-        int aID = (int) m.get("aID");
+        int aID = getLoggedInAccountID();
         boolean updateSuccessful = consentService.updateAccountConsent(aID, ConsentLevel.fromString(c));
         return "redirect:/settings?update="+updateSuccessful;
+    }
+
+    private int getLoggedInAccountID() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        MyUser userDetails = (MyUser)userService.loadUserByUsername(auth.getName());
+        return userDetails.getUser().getAccountId();
     }
 }
--- src/main/java/com/turboconsulting/Controller/VisitorsController.java
+++ src/main/java/com/turboconsulting/Controller/VisitorsController.java
@@ -1,7 +1,11 @@
 package com.turboconsulting.Controller;
 
+import com.turboconsulting.Security.MyUser;
+import com.turboconsulting.Security.MyUserDetailsService;
 import com.turboconsulting.Service.ConsentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -11,16 +15,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes("aID")
 public class VisitorsController {
 
     @Autowired
     private ConsentService consentService;
 
+    @Autowired
+    private MyUserDetailsService userService;
+
     @GetMapping("/visitors")
     public String visitorsPage(ModelMap m)  {
-        int aID = (int) m.get("aID");
+        int aID = getLoggedInAccountID();
         m.addAttribute("visitors", consentService.getAccountsVisitors(aID));
         return "visitors";
+    }
+
+    private int getLoggedInAccountID() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        MyUser userDetails = (MyUser)userService.loadUserByUsername(auth.getName());
+        return userDetails.getUser().getAccountId();
     }
 }
