bbae83b5981265d00277b6b87a329eccbba67f73
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-04-07 15:02:25 +0100

    moveAllToReviewed servce method and controller implemented

--- src/main/java/com/turboconsulting/Service/ConsentServiceInterface.java
+++ src/main/java/com/turboconsulting/Service/ConsentServiceInterface.java
@@ -13,30 +13,49 @@ public interface ConsentServiceInterface {
 
     //////////////////////////////////////////////////////////////////////////ACCOUNT FUNCTIONS
     boolean addNewAccount(Account a);
+
     int getAccountID(String email);
+
     boolean checkAccountLogin(LoginDetails loginDetails);
+
     Iterable<Visitor> getAccountsVisitors(int aID);
+
     Iterable<Account> getAllAccounts();
+
     Account getAccount(int id);
+
     boolean updateAccountConsent(int id, ConsentLevel c);
 
     //////////////////////////////////////////////////////////////////////////VISITOR FUNCTIONS
     boolean addNewVisitor(Visitor v, int accountID);
+
     Visitor getVisitor(int id);
+
     Iterable<Visitor> getAllVisitors();
+
     boolean updateVisitorConsent(int id, ConsentLevel c);
 
     //////////////////////////////////////////////////////////////////////////EXPERIMENT FUNCTIONS
     boolean addNewExperiment(Experiment e);
+
     Experiment getExperiment(int id);
+
     Iterable<Experiment> getAllExperiments();
 
     //////////////////////////////////////////////////////////////////////////VISITOR_EXPERIMENT FUNCTIONS
     Iterable<VisitorExperiment> getVisitorExperiments(int id);
+
     VisitorExperiment getVisitorExperiment(int visitorID, int experimentID);
+
     boolean doExperiment(int visitorId, int experimentId);
+
     String getExperimentConsent(int id, int experimentID);
+
     boolean updateExperimentConsent(int visitorId, ConsentLevel c, int experimentID);
-    public boolean updateBatchExperimentConsents(int visitorId, ConsentLevel c, List<Integer> experimentIds);
+
+    boolean updateBatchExperimentConsents(int visitorId, ConsentLevel c, List<Integer> experimentIds);
+
     int getPendingExperiments(int id);
+
+    boolean moveAllToReviewed(int visitorId);
 }
