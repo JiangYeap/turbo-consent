a4ec2ec60259a70d927e384faf670801333ff211
Unknown <fh16413@my.bristol.ac.uk>
2018-04-08 09:14:25 +0100

    Login working with database

--- src/main/java/com/turboconsulting/Security/SecurityConfig.java
+++ src/main/java/com/turboconsulting/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.turboconsulting.Security;
 
+import com.turboconsulting.DAO.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,14 +13,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    AccountDao accountDao;
+
+    @Autowired
+    public MyUserDetailsService userDetailsService() {
+        return new MyUserDetailsService(accountDao);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    /*@Autowired
     protected void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("hw16471@bristol.ac.uk").password("password").roles("USER");
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/login").permitAll()
-                .antMatchers("/", "/visitors/**", "/settings/**").access("hasRole('USER')")
+                .antMatchers("/", "/visitors/**", "/settings/**").hasAuthority("USER")
                 .and().formLogin().loginPage("/login");
     }
 }
