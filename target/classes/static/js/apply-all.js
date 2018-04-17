dbb7a8cc370e5cac736b3e0c176e25d3f46ed4ef
Unknown <fh16413@my.bristol.ac.uk>
2018-04-17 12:27:49 +0100

    Bug fix

--- target/classes/static/js/apply-all.js
+++
@@ -1,7 +0,0 @@
-// Implements apply all for /settings
-$(document).ready(function() {
-    $('.apply-master').click(function() {
-        let masterValue = $('.master-select').prop('selectedIndex');
-        $('.child-select').prop('selectedIndex', masterValue);
-    })
-});
--- target/classes/static/js/cc-entry.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.add-cc-entry').click(function() {
-        $('.cc-entry:lt(2)').clone().find('input, textarea').val('').end().appendTo('.add-exp');
-    });
-});
